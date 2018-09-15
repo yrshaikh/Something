7c8b93061d68b6325c9889bd984890d920c75a52
Yasser Shaikh <yasser.shaikh@agoda.com>
2018-09-15 23:20:06 +0700

    Added form and url.

--- Something/WebApplication/wwwroot/js/apps/authentication/components/LoginForm/LoginForm.tsx
+++ Something/WebApplication/wwwroot/js/apps/authentication/components/LoginForm/LoginForm.tsx
@@ -15,40 +15,44 @@ class SimpleForm extends React.Component<ILoginProps & FormComponentProps> {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                console.log("Received values of form: ", values);
+                e.currentTarget.submit();
             }
         });
     }
     render (): JSX.Element {
         const { getFieldDecorator } = this.props.form;
-        return (
-            <div className="pane ant-col-md-12">
-                <Form onSubmit={this.handleSubmit} className="LoginForm">
-                    <h2>Login to your account</h2>
-                    <b>Email address</b>
-                    <FormItem>
-                        {getFieldDecorator("userName", {
-                            rules: [{ required: true, message: "Please input your username!" }],
-                        })(
-                            <Input className="ant-input-lg" placeholder="Username" />
-                        )}
-                    </FormItem>
-                    <b>Password</b>
-                    <FormItem>
-                        {getFieldDecorator("password", {
-                            rules: [{ required: true, message: "Please input your Password!" }],
-                        })(
-                            <Input className="ant-input-lg" type="password" placeholder="Password" />
-                        )}
-                    </FormItem>
-                    <FormItem>
-                        <Button type="primary" htmlType="submit" className="login-form-button ovrd-btn">
-                            Log in
-                    </Button>
-                    </FormItem>
-                </Form>
-            </div>
-        );
+        return <div className="pane ant-col-md-12">
+        <Form action="/account/login" onSubmit={this.handleSubmit} className="LoginForm">
+            <h2>Login to your account</h2>
+            <b>Email address</b>
+            <FormItem>
+                {getFieldDecorator("userName", {
+                rules: [
+                    {
+                    required: true,
+                    message: "Please input your username!"
+                    }
+                ]
+                })(<Input className="ant-input-lg" placeholder="Username" />)}
+            </FormItem>
+            <b>Password</b>
+            <FormItem>
+                {getFieldDecorator("password", {
+                rules: [
+                    {
+                    required: true,
+                    message: "Please input your Password!"
+                    }
+                ]
+                })(<Input className="ant-input-lg" type="password" placeholder="Password" />)}
+            </FormItem>
+            <FormItem>
+                <Button type="primary" htmlType="submit" className="login-form-button ovrd-btn">
+                Log in
+                </Button>
+            </FormItem>
+            </Form>
+          </div>;
     }
 }
 
