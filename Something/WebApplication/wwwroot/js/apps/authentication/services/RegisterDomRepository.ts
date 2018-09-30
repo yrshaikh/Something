e856c05b7624e381f2f56efec4b6b3a4ea3616fb
Ali Rizvi <Ali.Rizvi@agoda.com>
2018-09-30 22:44:46 +0700

    cleaned some bad code

--- Something/WebApplication/wwwroot/js/apps/authentication/services/RegisterDomRepository.ts
+++ Something/WebApplication/wwwroot/js/apps/authentication/services/RegisterDomRepository.ts
@@ -1,27 +1,15 @@
 import { IAuthenticationDomRepository } from "./IAuthenticationDomRepository";
 
-export class RegisterDomRepository implements IAuthenticationDomRepository {
-    public getHeaderText = (): string => {
-        return "Create your account";
-    }
+export const RegisterDomRepository: IAuthenticationDomRepository = {
+    getHeaderText() { return "Create your account"; },
 
-    public getSubtitleText = (): string => {
-        return "psst! ...it's free forever";
-    }
+    getSubtitleText() { return "psst! ...it's free forever"; },
 
-    public getButtonText = (): string => {
-        return "Create an account";
-    }
+    getButtonText() { return "Create an account"; },
 
-    public getAlternateText = (): string => {
-        return "Have a account? Login";
-    }
+    getAlternateText() { return "Have a account? Login"; },
 
-    public getAlternateLink = (): string => {
-        return "/account/login";
-    }
+    getAlternateLink() { return "/account/login"; },
 
-    public getFormSubmitLink = (): string => {
-        return "/account/register";
-    }
+    getFormSubmitLink() { return "/account/register"; },
 }
