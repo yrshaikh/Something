a06aeb74e5ad5ef2f7f386e9543e4006136d0beb
Yasser Shaikh <yasser.shaikh@agoda.com>
2018-10-03 01:21:14 +0700

    fix.

---
+++ Something/WebApplication/wwwroot/js/apps/services/CompanyService.ts
@@ -0,0 +1,7 @@
+import { AjaxRequest } from "./AjaxRequest";
+
+export const CompanyService = {
+    post(companyName: string): Promise<any> {
+        return AjaxRequest.post("/company/create", { companyName });
+    },
+};
--- Something/WebApplication/wwwroot/js/apps/sprint/components/CreateSprint.tsx
+++ Something/WebApplication/wwwroot/js/apps/sprint/components/CreateSprint.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
 
-import './CreateSprint.scss';
+import "./CreateSprint.scss";
 
 export class CreateSprint extends React.Component<CreateSprint.Props, CreateSprint.State> {
     public render(): JSX.Element {
         return (
             <h2>Create Sprint</h2>
-        )
+        );
     }
 }
