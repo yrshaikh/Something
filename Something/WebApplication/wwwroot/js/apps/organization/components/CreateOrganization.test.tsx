02ad5b917fce7ea6877a51e52da3e8d33d651865
Yasser Shaikh <yasser.shaikh@agoda.com>
2018-10-13 18:20:09 +0700

    Added tests for CreateOrganization.tsx.

---
+++ Something/WebApplication/wwwroot/js/apps/organization/components/CreateOrganization.test.tsx
@@ -0,0 +1,20 @@
+import "jest";
+import * as React from "react";
+import { shallow, mount } from "enzyme";
+import enzymeToJson from "enzyme-to-json";
+
+import { CreateOrganization } from "./CreateOrganization";
+
+describe("<CreateOrganization />", () => {
+    it("should render correctly", () => {
+        const wrapper = shallow(<CreateOrganization isOnboarding={true} onCreateCallback={null} />);
+        expect(enzymeToJson(wrapper)).toMatchSnapshot();
+    });
+
+    it("should enable button when text is entered", () => {
+        const wrapper = shallow(<CreateOrganization isOnboarding={true} onCreateCallback={null} />);
+        expect(wrapper.find('.ant-btn--disabled').length).toBe(1);
+        wrapper.setState({ organizationName: 'howdy' });
+        expect(wrapper.find('.ant-btn--disabled').length).toBe(0);
+    });
+});
