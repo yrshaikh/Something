6fdf1aadd0930890e434e1b07bfc2dc72268ccc8
Yasser Shaikh <yasser.shaikh@agoda.com>
2018-10-23 18:00:51 +0700

    Some colors to the homepage.

--- Something/WebApplication/wwwroot/js/apps/home/Home.tsx
+++ Something/WebApplication/wwwroot/js/apps/home/Home.tsx
@@ -8,18 +8,20 @@ export const Home : React.SFC<IHomeProps> = () => {
     return <div className="Home">
         <h1>Something.</h1>
         <p>An open source project and issue management app that doesn't suck!</p>
-        <div className="Home__btnHolder">
-            <Button
-                size="large"
-                type="primary"
+        <div className="Home__btns">
+            <a
+                className="Home__btns--default Home__btns--yellow"
                 href="/account/login">
                 Login
-            </Button>
-            <Button
-                size="large"
+            </a>
+            <a
+                className="Home__btns--default Home__btns--yellow"
                 href="/account/register">
                 Register
-            </Button>
+            </a>
+        </div>
+        <div className="Home__gallery">
+            <img src="https://cdn.dribbble.com/users/399068/screenshots/5374838/2018-10-10_155047.png" />
         </div>
     </div>;
 }
