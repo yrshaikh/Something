c98b552c8127a331db59cf1f4b6035140691b9aa
Yasser <yasser.shaikh@agoda.com>
2019-03-24 18:30:48 +0700

    Fixes.

--- Something/WebApplication/wwwroot/js/apps/authentication/Authentication.tsx
+++ Something/WebApplication/wwwroot/js/apps/authentication/Authentication.tsx
@@ -1,17 +1,13 @@
 import * as React from "react";
 import { AuthenticationForm } from "./components/AuthenticationForm/AuthenticationForm";
-import { Splash } from "./components/Splash/Splash";
 import { IAuthenticationProps } from "./types/IAuthenticationProps";
 
 import "./Authentication.scss";
 
 export const Authentication: React.SFC<IAuthenticationProps> = (props: IAuthenticationProps) => {
     return <div className="authentication ant-row">
-        <div className="ant-col-md-12 authentication__pane">
+        <div className="ant-col-md-12 ant-col-offset-6 authentication__pane">
             <AuthenticationForm {...props} />
-        </div>
-        <div className="ant-col-md-12 authentication__pane">
-            <Splash pageType={props.pageTypeId} />
         </div>
     </div>;
 };
